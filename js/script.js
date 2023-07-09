bdf9ffdb9dab1bd69002d8f4ad85e623278aaa8c
reil <maxim.baskakov@bk.ru>
2023-07-09 03:32:03 +0300

    add smooth scrolling links

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const burger = document.querySelector('.burger'),
     menu = document.querySelector('.menu'),
     closeElem = document.querySelector('.menu__close');
+    menuLinks = document.querySelectorAll(".menu__link");
+
 
 burger.addEventListener('click', () =>{
     menu.classList.add('active');
@@ -8,6 +10,12 @@ burger.addEventListener('click', () =>{
 closeElem.addEventListener('click', () =>{
     menu.classList.remove('active');
 });
+menuLinks.forEach((link) =>
+  link.addEventListener("click", () => {
+    menu.classList.remove("active");
+    burger.classList.remove("active");
+  })
+);
 
 const counter = document.querySelectorAll('.skills__ratings-counter')
     lines = document.querySelectorAll('.skills__ratings-line div')
